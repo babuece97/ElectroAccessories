06c49ed394b0ffadcd510671f4d3393f0305b1ed
babuece97 <babuece97@yahoo.com.com>
2022-01-30 04:59:36 +0100

    banner and nav bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './component/Home/Home';
+
+const App=()=> {
+  return (
+    <div >
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StyledComponent/MuiButton.js
@@ -0,0 +1,10 @@
+import {styled} from "@mui/system";
+import { Button } from "@mui/material";
+
+
+const MuiButton = styled(Button) ({
+    background:'liner-gradient (90deg, #12EEEAE, #0FCFEC)',
+    color:'#7cfc00'
+})
+
+export default MuiButton;
